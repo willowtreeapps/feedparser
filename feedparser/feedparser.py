02f01a3d42b228d67a37f33b6520127ab7a8f321
Mike Wilkerson <mike.wilkerson@willowtreeapps.com>
2015-02-03 17:01:33 -0500

    Adding support for multiple dc:creator elements

--- feedparser/feedparser.py
+++ feedparser/feedparser.py
@@ -536,10 +536,10 @@ class _FeedParserMixin:
         self.incontributor = 0
         self.inpublisher = 0
         self.insource = 0
-        
+
         # georss
         self.ingeometry = 0
-        
+
         self.sourcedata = FeedParserDict()
         self.contentparams = FeedParserDict()
         self._summaryKey = None
@@ -1144,6 +1144,10 @@ class _FeedParserMixin:
         context = self._getContext()
         context.setdefault('authors', [])
         context['authors'].append(FeedParserDict())
+        # Reset the author and author_detail keys in the context, in the case
+        # this is the second author encountered.
+        context.pop('author', None)
+        context.pop('author_detail', None)
     _start_managingeditor = _start_author
     _start_dc_author = _start_author
     _start_dc_creator = _start_author
@@ -1153,6 +1157,7 @@ class _FeedParserMixin:
         self.pop('author')
         self.inauthor = 0
         self._sync_author_detail()
+        self._push_detail_to_authors()
     _end_managingeditor = _end_author
     _end_dc_author = _end_author
     _end_dc_creator = _end_author
@@ -1278,8 +1283,14 @@ class _FeedParserMixin:
         context.setdefault(prefix + '_detail', FeedParserDict())
         context[prefix + '_detail'][key] = value
         self._sync_author_detail()
+
+    def _push_detail_to_authors(self, prefix='author'):
+        context = self._getContext()
         context.setdefault('authors', [FeedParserDict()])
-        context['authors'][-1][key] = value
+        if prefix + '_detail' in context:
+            context['authors'][-1] = FeedParserDict(
+                context[prefix + '_detail']
+            )
 
     def _save_contributor(self, key, value):
         context = self._getContext()
@@ -1448,7 +1459,7 @@ class _FeedParserMixin:
         geometry = _parse_georss_line(self.pop('geometry'))
         if geometry:
             self._save_where(geometry)
-    
+
     def _end_georss_polygon(self):
         this = self.pop('geometry')
         geometry = _parse_georss_polygon(this)
@@ -1904,11 +1915,11 @@ class _FeedParserMixin:
             self.psc_chapters_flag = True
             attrsD['chapters'] = []
             self._getContext()['psc_chapters'] = FeedParserDict(attrsD)
-            
+
     def _end_psc_chapters(self):
         # Transition from True -> False
         self.psc_chapters_flag = False
-        
+
     def _start_psc_chapter(self, attrsD):
         if self.psc_chapters_flag:
             start = self._getAttribute(attrsD, 'start')
@@ -2813,7 +2824,7 @@ try:
     del regex
 except NameError:
     pass
-    
+
 def _parse_date_iso8601(dateString):
     '''Parse a variety of ISO-8601-compatible formats like 20040105'''
     m = None
@@ -3055,7 +3066,7 @@ def _parse_date_w3dtf(datestr):
             parts.append('00:00:00z')
     elif len(parts) > 2:
         return None
-    date = parts[0].split('-', 2) 
+    date = parts[0].split('-', 2)
     if not date or len(date[0]) != 4:
         return None
     # Ensure that `date` has 3 elements. Using '1' sets the default
@@ -3396,7 +3407,7 @@ def convert_to_utf8(http_headers, data):
                                  u'application/xml-external-parsed-entity')
     text_content_types = (u'text/xml', u'text/xml-external-parsed-entity')
     if (http_content_type in application_content_types) or \
-       (http_content_type.startswith(u'application/') and 
+       (http_content_type.startswith(u'application/') and
         http_content_type.endswith(u'+xml')):
         acceptable_content_type = 1
         rfc3023_encoding = http_encoding or xml_encoding or u'utf-8'
@@ -3583,7 +3594,7 @@ def _parse_georss_polygon(value, swap=True, dims=2):
     # A polygon contains a space separated list of latitude-longitude pairs,
     # with each pair separated by whitespace. There must be at least four
     # pairs, with the last being identical to the first (so a polygon has a
-    # minimum of three actual points). 
+    # minimum of three actual points).
     try:
         ring = list(_gen_georss_coords(value, swap, dims))
     except (IndexError, ValueError):
